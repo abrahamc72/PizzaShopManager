24722a0a1e65f01377012f9332e7b393e382554b
abrahamc72 <acerva25@asu.edu>
2024-02-16 16:04:05 -0700

    fixed bat

--- createvenv.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM Get the directory of the current batch file
-setlocal
-cd /d %~dp0
-REM Create the virtual environment named 'venv' in the same directory
-python3 -m venv venv
-echo Virtual environment created successfully in the current directory.
-endlocal
--- installbuildclient.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Installing React project dependencies...
-npm install
-echo Building the React project...
-npm run build
-echo React project built successfully.
-pause
